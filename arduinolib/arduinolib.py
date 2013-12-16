53e3e2e33eab711e62415c13d143b38884fefb26
Alejandro Guirao <lekumberri@gmail.com>
2013-12-16 17:04:10 +0100

    Implemented analog/digital read/write in the python lib

--- arduinolib/arduinolib.py
+++ arduinolib/arduinolib.py
@@ -29,7 +29,41 @@ class Arduino():
         header, value = line_received.split(':') # e.g. D13:1
         if header == ('D'+ str(pin_number)):
             # If header matches
-            return value
+            return int(value)
+
+    def digital_write(self, pin_number, digital_value):
+        """
+        Writes the digital_value on pin_number
+        Internally sends b'WD{pin_number}:{digital_value}' over the serial
+        connection
+        """
+        command = (''.join(('WD', str(pin_number), ':',
+            str(digital_value)))).encode()
+        self.conn.write(command) 
+     
+    def analog_read(self, pin_number):
+        """
+        Performs an analog read on pin_number and returns the value (0 to 1023)
+        Internally sends b'RA{pin_number}' over the serial connection
+        """
+        command = (''.join(('RA', str(pin_number)))).encode()
+        self.conn.write(command) 
+        line_received = self.conn.readline().decode().strip()
+        header, value = line_received.split(':') # e.g. A4:1
+        if header == ('A'+ str(pin_number)):
+            # If header matches
+            return int(value)
+
+    def analog_write(self, pin_number, analog_value):
+        """
+        Writes the analog value (0 to 255) on pin_number
+        Internally sends b'WA{pin_number}:{analog_value}' over the serial
+        connection
+        """
+        command = (''.join(('WA', str(pin_number), ':',
+            str(analog_value)))).encode()
+        self.conn.write(command) 
+
 
 if __name__ == '__main__':
     
@@ -37,4 +71,8 @@ if __name__ == '__main__':
 
     a = Arduino()
     time.sleep(5)
-    print(a.digital_read(13))
+    a.digital_write(13,1)
+    a.analog_write(5,245)
+    time.sleep(2)
+    print(a.digital_read(12))
+    print(a.analog_read(2))
