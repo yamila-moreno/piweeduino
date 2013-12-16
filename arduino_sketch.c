96c7fd3b09569c862848af93cd95e83954f68490
Yamila Moreno <yamila.moreno@kaleidos.net>
2013-12-16 15:36:54 +0100

    Format changes in C sketch

--- arduino_sketch.c
+++ arduino_sketch.c
@@ -1,11 +1,11 @@
 /*
  * Sketch to control the pins of Arduino via serial interface
  * Developed for piweek dec 2013
- * 
+ *
  *
  * Commands implemented with examples:
  *
- * - RD13 -> Reads the Digital input at pin 13 
+ * - RD13 -> Reads the Digital input at pin 13
  * - RA4 - > Reads the Analog input at pin 4
  * - WD13:1 -> Writes 1 (HIGH) to digital output pin 13
  * - WA6:125 -> Writes 125 to analog output pin 6 (PWM)
@@ -23,89 +23,87 @@ int wait_for_transmission = 5; // Delay in ms in order to receive the serial dat
 
 
 void digital_read(int pin_number){
-        /*
-         * Performs a digital read on pin_number and returns the value read to serial
-         * in this format: D{pin_number}:{value}\n where value can be 0 or 1     
-         */
+    /*
+     * Performs a digital read on pin_number and returns the value read to serial
+     * in this format: D{pin_number}:{value}\n where value can be 0 or 1
+     */
 
-        pinMode(pin_number, INPUT);
-        digital_value = digitalRead(pin_number);
-        Serial.print('D');
-        Serial.print(pin_number);
-        Serial.print(':'); 
-        Serial.println(digital_value); // Adds a trailing \n 
+    pinMode(pin_number, INPUT);
+    digital_value = digitalRead(pin_number);
+    Serial.print('D');
+    Serial.print(pin_number);
+    Serial.print(':');
+    Serial.println(digital_value); // Adds a trailing \n
 }
 
 void analog_read(int pin_number){
-        /*
-         * Performs an analog read on pin_number and returns the value read to serial
-         * in this format: A{pin_number}:{value}\n where value ranges from 0 to 1023    
-         */
+    /*
+     * Performs an analog read on pin_number and returns the value read to serial
+     * in this format: A{pin_number}:{value}\n where value ranges from 0 to 1023
+     */
 
-        analog_value = analogRead(pin_number);
-        Serial.print('A');
-        Serial.print(pin_number);
-        Serial.print(':'); 
-        Serial.println(analog_value); // Adds a trailing \n 
+    analog_value = analogRead(pin_number);
+    Serial.print('A');
+    Serial.print(pin_number);
+    Serial.print(':');
+    Serial.println(analog_value); // Adds a trailing \n
 }
 
 void digital_write(int pin_number, int digital_value){
-        /*
-	 * Performs a digital write on pin_number with the digital_value
-	 * The value must be 1 or 0 
-         */
-        pinMode(pin_number, OUTPUT);
+    /*
+     * Performs a digital write on pin_number with the digital_value
+     * The value must be 1 or 0
+     */
+    pinMode(pin_number, OUTPUT);
 	digitalWrite(pin_number, digital_value);
 }
 
 void analog_write(int pin_number, int analog_value){
-        /*
+    /*
 	 * Performs an analog write on pin_number with the analog_value
-	 * The value must be range from 0 to 255 
-         */
+	 * The value must be range from 0 to 255
+     */
 	analogWrite(pin_number, analog_value);
-} 
+}
 
 void setup() {
-        Serial.begin(9600); // Serial Port at 9600 baud
+    Serial.begin(9600); // Serial Port at 9600 baud
 }
 
 
 void loop() {
-        
-        // Check if characters available in the buffer
-        if (Serial.available() > 0) {
-                operation = Serial.read();
-		delay(wait_for_transmission); // If not delayed, second character is not correctly read
-		mode = Serial.read();
-		pin_number = Serial.parseInt(); // Waits for an int to be transmitted, if too slow,
-	       					// could also be tuned with Serial.setTimeout()	
-		if (Serial.read()==':'){
-			value_to_write = Serial.parseInt(); // Collects the value to be written
-		}
-                switch (operation){
-                        case 'R': // Read operation, e.g. RD12, RA4
-                                if (mode == 'D'){ // Digital read 
-                                        digital_read(pin_number); 
-                                } else if (mode == 'A'){ // Analog read
-                                        analog_read(pin_number);
+    // Check if characters available in the buffer
+    if (Serial.available() > 0) {
+        operation = Serial.read();
+        delay(wait_for_transmission); // If not delayed, second character is not correctly read
+        mode = Serial.read();
+        pin_number = Serial.parseInt(); // Waits for an int to be transmitted, if too slow,
+        // could also be tuned with Serial.setTimeout()
+        if (Serial.read()==':'){
+            value_to_write = Serial.parseInt(); // Collects the value to be written
+        }
+        switch (operation){
+            case 'R': // Read operation, e.g. RD12, RA4
+                if (mode == 'D'){ // Digital read
+                    digital_read(pin_number);
+                } else if (mode == 'A'){ // Analog read
+                    analog_read(pin_number);
 				} else {
 					break; // Unexpected mode
 				}
-                                break; 
-                                 
-			case 'W': // Write operation, e.g. WD3:1, WA8:255
-				if (mode == 'D'){ // Digital write
-					 digital_write(pin_number, value_to_write);
-                                } else if (mode == 'A'){ // Analog write
-					 analog_write(pin_number, value_to_write);
-				} else {
-					break; // Unexpected mode
-				}
-                                break; 
-                                 
-                        default: // Unexpected char
-                                break;
-		}
-	}
+                break;
+            case 'W': // Write operation, e.g. WD3:1, WA8:255
+                if (mode == 'D'){ // Digital write
+                    digital_write(pin_number, value_to_write);
+                } else if (mode == 'A'){ // Analog write
+                    analog_write(pin_number, value_to_write);
+                } else {
+                    break; // Unexpected mode
+                }
+                break;
+
+            default: // Unexpected char
+                break;
+        }
+    }
 }
